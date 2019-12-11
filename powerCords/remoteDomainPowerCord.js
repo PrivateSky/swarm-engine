115e9a5b70fc9abbfef906e32433b9ff9634c896
asac <asac.cool@gmail.com>
2019-12-11 17:15:18 +0200

    first implementation of remoteDomainPowerCord (it uses branch just-refactoring from psk-http-client module)

---
+++ powerCords/remoteDomainPowerCord.js
@@ -0,0 +1,41 @@
+const outbound = "outbound";
+const inbound = "inbound";
+
+function RemoteDomainPowerCord(host, channelName, receivingHost, receivingChannelName){
+
+    receivingHost = receivingHost || host;
+    receivingChannelName = receivingChannelName || generateChannelName();
+
+    function setup(){
+        //injecting necessary http methods
+        require("../../psk-http-client");
+
+        //this should be a channel that exists... we don't try to create
+        $$.remote.registerPskHttpClient(outbound, host, channelName, {autoCreate: false});
+
+        //maybe instead of receivingChannelName we sould use our identity? :-??
+        $$.remote.registerPskHttpClient(inbound, receivingHost, receivingChannelName, {autoCreate: true});
+
+        $$.remote[outbound].setSenderMode();
+        $$.remote[inbound].setReceiverMode.call(this);
+    }
+
+    this.sendSwarm = function (swarmSerialization) {
+        $$.remote[outbound].sendSwarm(swarmSerialization);
+    };
+
+    function generateChannelName(){
+        return Math.random().toString(36).substr(2, 9);
+    }
+
+    return new Proxy(this, {
+        set(target, p, value, receiver) {
+            target[p] = value;
+            if(p === 'identity') {
+                setup();
+            }
+        }
+    });
+}
+
+module.exports = RemoteDomainPowerCord;
