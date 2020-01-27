cd3f9f0cc2c4aec1d577eaeed70a8e9eaf7628bb
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-01-27 18:02:46 +0200

    moved edfs fetch strategy to host boot scripts

--- bootScripts/browser/host/index.js
+++ bootScripts/browser/host/index.js
@@ -1,6 +1,12 @@
 HostBootScript = require("./HostBootScript");
-let bootScript = new HostBootScript();
+let bootScript = null;
 module.exports = {
+    initializeBootScript : function(seed){
+        if(bootScript){
+            throw new Error("HostBootScript already initialized");
+        }
+       bootScript = new HostBootScript(seed);
+    },
     getBootScriptLoader:function(){
         return bootScript;
     }
