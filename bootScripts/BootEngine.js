8160cad4e91a724c7043d341a06069e23595aee6
skutner <sava.dumitru.daniel@gmail.com>
2020-03-24 17:13:04 +0200

    small fix in FolderBarMap.listFiles

--- bootScripts/BootEngine.js
+++ bootScripts/BootEngine.js
@@ -30,9 +30,9 @@ function BootEngine(getSeed, getEDFS, initializeSwarmEngine, runtimeBundles, con
         const listFiles = promisify(this.rawDossier.listFiles);
         const readFile = promisify(this.rawDossier.readFile);
 
-        let fileList = await listFiles("/"+EDFS.constants.CSB.CONSTITUTION_FOLDER);
-        fileList = bundles.filter(bundle => fileList.includes(`${EDFS.constants.CSB.CONSTITUTION_FOLDER}/${bundle}`))
-            .map(bundle => `${EDFS.constants.CSB.CONSTITUTION_FOLDER}/${bundle}`);
+        let fileList = await listFiles("/" + EDFS.constants.CSB.CONSTITUTION_FOLDER);
+        fileList = bundles.filter(bundle => fileList.includes(`/${bundle}`))
+            .map(bundle => `/${bundle}`);
 
         if (fileList.length !== bundles.length) {
             const message = `Some bundles missing. Expected to have ${JSON.stringify(bundles)} but got only ${JSON.stringify(fileList)}`;
