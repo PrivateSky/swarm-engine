ac551b427a2db36e70eb0df8ad5b064aa0eadd23
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-03-28 00:30:07 +0200

    The handler set() method should return a boolean value

---
+++ bootScripts/browser/host/HostBootScript.js
@@ -0,0 +1,12 @@
+function SSAppBootScript(identity){
+    const se = require("swarm-engine");
+    se.initialise(identity);
+    const SRPC = se.SmartRemoteChannelPowerCord;
+    let swUrl = "http://localhost:8080/";
+    const powerCord = new SRPC([swUrl]);
+    $$.swarmEngine.plug("test/agent/007", powerCord);
+}
+
+module.exports = SSAppBootScript;
+
+
