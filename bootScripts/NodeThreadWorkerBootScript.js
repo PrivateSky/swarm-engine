d4971504ed942e838fd8c836047132b4eadcc622
Daniel Visoiu <daniel.visoiu@toptal.com>
2020-12-09 22:20:40 +0200

    Added support for download middleware inside Node thread worker.

--- bootScripts/NodeThreadWorkerBootScript.js
+++ bootScripts/NodeThreadWorkerBootScript.js
@@ -132,8 +132,50 @@ function boot() {
                 return;
             }
 
+            if (requestedPath.indexOf("/download") === 0) {
+                function extractPath() {
+                    let path = requestedPath.split("/").slice(2); // remove the "/delete" or "/download" part
+                    path = path.filter((segment) => segment.length > 0).map((segment) => decodeURIComponent(segment));
+                    return path;
+                }
+
+                let path = extractPath();
+                if (!path.length) {
+                    res.statusCode = 404;
+                    return res.end("File not found");
+                }
+                path = `/${path.join("/")}`;
+
+                dsu.readFile(path, (err, stream) => {
+                    if (err) {
+                        if (err instanceof Error) {
+                            if (err.message.indexOf("could not be found") !== -1) {
+                                res.statusCode = 404;
+                                return res.end("File not found");
+                            }
+
+                            res.statusCode = 500;
+                            return res.end(err.message);
+                        }
+
+                        res.statusCode = 500;
+                        return res.end(Object.prototype.toString.call(err));
+                    }
+
+                    // Extract the filename
+                    const filename = path.split("/").pop();
+
+                    let fileExt = filename.substring(filename.lastIndexOf(".") + 1);
+                    res.setHeader("Content-Type", MimeType.getMimeTypeFromExtension(fileExt).name);
+                    res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
+                    res.statusCode = 200;
+                    res.end(stream);
+                });
+                return;
+            }
+
             // handling file request
-            let file = requestedPath;
+            let file = requestedPath.split("?")[0]; // remove query params since this is a file request
             if (!file || file === "/" || file.indexOf(".") === -1) {
                 file = "/index.html";
             }
@@ -156,7 +198,10 @@ function boot() {
 
                     if (["htm", "html", "xhtml"].includes(fileExtension)) {
                         const baseUrl = `${url.substr(0, url.indexOf("/iframe"))}/iframe/${seed}/`;
-                        content = content.replace("PLACEHOLDER_THAT_WILL_BE_REPLACED_BY_SW_OR_SERVER_WITH_BASE_TAG", `<base href="${baseUrl}">`);
+                        content = content.replace(
+                            "PLACEHOLDER_THAT_WILL_BE_REPLACED_BY_SW_OR_SERVER_WITH_BASE_TAG",
+                            `<base href="${baseUrl}">`
+                        );
                     }
                 }
 
