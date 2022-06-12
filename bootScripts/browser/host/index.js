36130d2d3782231e3ab9a74378e978ee548a121b
Daniel Visoiu <9691701+daniel-visoiu@users.noreply.github.com>
2022-06-12 17:35:49 +0300

    Replaced URL logic for iframe with cloud-wallet

--- bootScripts/browser/host/index.js
+++ bootScripts/browser/host/index.js
@@ -1,8 +1,8 @@
 function setSSAppContext() {
     const baseUrl = typeof document !== "undefined" ? (document.getElementsByTagName("base")[0] || {}).href : undefined;
     let seed;
-    if (baseUrl && baseUrl.indexOf("/iframe/") !== -1) {
-        seed = baseUrl.substr(baseUrl.indexOf("/iframe/") + "/iframe/".length);
+    if (baseUrl && baseUrl.indexOf("/cloud-wallet/") !== -1) {
+        seed = baseUrl.substr(baseUrl.indexOf("/cloud-wallet/") + "/cloud-wallet/".length);
         const seedMatch = seed.match(/^([\w\d]*).*$/);
         seed = seedMatch ? seedMatch[1] : null;
 
